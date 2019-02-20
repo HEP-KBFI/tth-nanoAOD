a76225194c736693b029a70a49986727a01cadf4
ktht <karlehataht@gmail.com>
2019-02-20 10:58:52 +0200

    Update launchall_nanoaod.sh

--- scripts/launchall_nanoaod.sh
+++ scripts/launchall_nanoaod.sh
@@ -4,10 +4,6 @@
 
 # GT choices based on: https://twiki.cern.ch/twiki/bin/viewauth/CMS/PdmVAnalysisSummaryTable
 
-#TODO give NANO_CFG a default value (and put it to /hdfs/store/$USER/nanoCfgs/<version>/<date>/nano_$TYPE_$ERA_cfg.py)
-#TODO give DATASET_FILE a default value based on $JOB_TYPE, $ERA and $IS_PRIVATE flags
-#TODO remove automatic deduction of sample type, rather check $PRIVATE_MINIAOD_PATH to decide whether the samples are private
-
 export JSON_FILE_2016="Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt"
 export JSON_FILE_2017="Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt"
 export JSON_FILE_2018="" #TBA
@@ -16,27 +12,32 @@ export AUTOCOND_DATA_2016_v2="80X_dataRun2_2016LegacyRepro_v4"
 export AUTOCOND_MC_2016_v2="80X_mcRun2_asymptotic_2016_TrancheIV_v8"
 export ERA_ARGS_2016_v2="Run2_2016,run2_miniAOD_80XLegacy"
 export ERA_KEY_2016_v2="2016v2"
+export DATASET_ERA_2016_v2="RunIISummer16MiniAODv2"
 
 export AUTOCOND_DATA_2016_v3="94X_dataRun2_v10"
 export AUTOCOND_MC_2016_v3="94X_mcRun2_asymptotic_v3"
 export ERA_ARGS_2016_v3="Run2_2016,run2_nanoAOD_94X2016"
 export ERA_KEY_2016_v3="2016v3"
+export DATASET_ERA_2016_v3="RunIISummer16MiniAODv3"
 
 # these GTs were taken from previous iteration of the analysis; no recommendation found!
 export AUTOCOND_DATA_2017_v1="94X_dataRun2_v6"
 export AUTOCOND_MC_2017_v1="94X_mc2017_realistic_v14"
 export ERA_ARGS_2017_v1="Run2_2017,run2_nanoAOD_94XMiniAODv1"
 export ERA_KEY_2017_v1="2017v1"
+export DATASET_ERA_2017_v1="RunIIFall17MiniAOD"
 
 export AUTOCOND_DATA_2017_v2="94X_dataRun2_v11"
 export AUTOCOND_MC_2017_v2="94X_mc2017_realistic_v17"
 export ERA_ARGS_2017_v2="Run2_2017,run2_nanoAOD_94XMiniAODv2"
 export ERA_KEY_2017_v2="2017v2"
+export DATASET_ERA_2017_v2="RunIIFall17MiniAODv2"
 
 export AUTOCOND_DATA_2018="102X_dataRun2_Sep2018Rereco_v1"
 export AUTOCOND_MC_2018="102X_upgrade2018_realistic_v12"
 export ERA_ARGS_2018="Run2_2018"
 export ERA_KEY_2018="2018"
+export DATASET_ERA_2018="RunIIAutumn18MiniAOD"
 
 OPTIND=1 # reset in case getopts has been used previously in the shell
 
@@ -51,7 +52,7 @@ TYPE_MC="mc"
 TYPE_FAST="fast"
 
 show_help() {
-  echo "Usage: $0 -e <era> [-d] [-g | -f <dataset file>] [-c <cfg>] [-j <type>] [-v version] [-w whitelist] [-p path]" 1>&2;
+  echo "Usage: $0 -e <era>  -j <type> [-d] [-g] [-f <dataset file>] [-c <cfg>] [-v version] [-w whitelist]" 1>&2;
   echo "Available eras: $ERA_KEY_2016_v2, $ERA_KEY_2016_v3, $ERA_KEY_2017_v1, $ERA_KEY_2017_v2, $ERA_KEY_2018" 1>&2;
   echo "Available job types: $TYPE_DATA, $TYPE_MC, $TYPE_FAST"
   exit 0;
@@ -77,8 +78,6 @@ while getopts "h?dgf:c:j:e:v:w:p:" opt; do
      ;;
   w) export WHITELIST=${OPTARG}
      ;;
-  p) export PRIVATE_MINIAOD_PATH=${OPTARG}
-     ;;
   esac
 done
 
@@ -91,61 +90,77 @@ check_if_exists() {
 
 if [ -z "$ERA" ]; then
   echo "You need to specify era!";
-  exit 6;
+  exit 1;
 fi
 
 if [ "$ERA" == "$ERA_KEY_2016_v2" ]; then
   export AUTOCOND_DATA=$AUTOCOND_DATA_2016_v2
   export AUTOCOND_MC=$AUTOCOND_MC_2016_v2
   export ERA_ARGS=$ERA_ARGS_2016_v2
+  export DATASET_ERA=$DATASET_ERA_2016_v2
   export JSON_FILE=$JSON_FILE_2016
   export YEAR="2016"
 elif [ "$ERA" == "$ERA_KEY_2016_v3" ]; then
   export AUTOCOND_DATA=$AUTOCOND_DATA_2016_v3
   export AUTOCOND_MC=$AUTOCOND_MC_2016_v3
   export ERA_ARGS=$ERA_ARGS_2016_v3
+  export DATASET_ERA=$DATASET_ERA_2016_v3
   export JSON_FILE=$JSON_FILE_2016
   export YEAR="2016"
 elif [ "$ERA" == "$ERA_KEY_2017_v1" ]; then
   export AUTOCOND_DATA=$AUTOCOND_DATA_2017_v1
   export AUTOCOND_MC=$AUTOCOND_MC_2017_v1
   export ERA_ARGS=$ERA_ARGS_2017_v1
+  export DATASET_ERA=$DATASET_ERA_2017_v1
   export JSON_FILE=$JSON_FILE_2017
   export YEAR="2017"
 elif [ "$ERA" == "$ERA_KEY_2017_v2" ]; then
   export AUTOCOND_DATA=$AUTOCOND_DATA_2017_v2
   export AUTOCOND_MC=$AUTOCOND_MC_2017_v2
   export ERA_ARGS=$ERA_ARGS_2017_v2
+  export DATASET_ERA=$DATASET_ERA_2017_v2
   export JSON_FILE=$JSON_FILE_2017
   export YEAR="2017"
 elif [ "$ERA" == "$ERA_KEY_2018" ]; then
   export AUTOCOND_DATA=$AUTOCOND_DATA_2018
   export AUTOCOND_MC=$AUTOCOND_MC_2018
   export ERA_ARGS=$ERA_ARGS_2018
+  export DATASET_ERA=$DATASET_ERA_2018
   export JSON_FILE=$JSON_FILE_2018
   export YEAR="2018"
   echo "Era $ERA yet not supported";
-  exit 0;
+  exit 1;
 else
   echo "Invalid era: $ERA";
 fi
+
+export BASE_DIR="$CMSSW_BASE/src/tthAnalysis/NanoAOD"
+export JSON_LUMI="$BASE_DIR/data/$JSON_FILE"
+export CRAB_CFG="$BASE_DIR/test/crab_cfg.py"
 
 if [ "$JOB_TYPE" != "$TYPE_DATA" ] && [ "$JOB_TYPE" != "$TYPE_MC" ] && [ "$JOB_TYPE" != "$TYPE_FAST" ]; then
   echo "Invalid job type: $JOB_TYPE";
   exit 1;
 fi
 
+if [ ! -z "$DATASET_FILE" ]; then
+  export DATASET_FILE="$BASE_DIR/test/datasets/txt/datasets_${JOB_TYPE}_${YEAR}_${DATASET_ERA}.txt";
+  read -p "Sure you want to run NanoAOD production on samples defined in $DATASET_FILE? " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+  fi
+fi
+
+check_if_exists "$DATASET_FILE"
+
 if [ -z "$NANOAOD_VER" ]; then
-  export NANOAOD_VER="NanoAOD_$ERA_`date '+%Y%b%d'`";
+  export NANOAOD_VER="NanoAOD_${ERA}_`date '+%Y%b%d'`";
 fi
 
 if [ ! -z "$YEAR" ]; then
   echo "Year not set";
   exit 1;
 fi
-
-export JSON_LUMI="$CMSSW_BASE/src/tthAnalysis/NanoAOD/data/$JSON_FILE"
-export CRAB_CFG="$CMSSW_BASE/src/tthAnalysis/NanoAOD/test/crab_cfg.py"
 
 generate_cfgs() {
   export CUSTOMISE_COMMANDS_DATA="process.MessageLogger.cerr.FwkReport.reportEvery = 1000\\n\
@@ -175,24 +190,27 @@ from tthAnalysis.NanoAOD.addLeptonSubtractedAK8Jets import addLeptonSubtractedAK
     CMSDRIVER_OPTS="$CMSDRIVER_OPTS --lumiToProcess=$JSON_LUMI";
   fi
 
-  echo "Generating the skeleton configuration file for CRAB $JOB_TYPE jobs: $NANOCFG"
-  cmsDriver.py $CMSDRIVER_OPTS
+  echo "Generating the skeleton configuration file for CRAB $JOB_TYPE jobs: $NANOCFG";
+  cmsDriver.py $CMSDRIVER_OPTS;
 }
 
-if [ -z "$DATASET_FILE" ]; then
-  if [ $GENERATE_CFGS_ONLY = true ]; then
-    generate_cfgs;
-    exit 0;
-  else
-    echo "You must provide the dataset file!";
-    exit 2;
+if [ $GENERATE_CFGS_ONLY = true ] || [ ! -z "$NANOCFG" ]; then
+  if [ ! -z "$NANOCFG" ]; then
+    export NANOCFG="$BASE_DIR/test/cfgs/nano_${JOB_TYPE}_${DATASET_ERA}_cfg.py";
+    read -p "Sure you want to use this config file: $NANOCFG? " -n 1 -r
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+      [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    fi
+    if [ ! -d $(dirname $NANOCFG) ]; then
+      mkdir -p $(dirname $NANOCFG);
+    fi
   fi
+  generate_cfgs;
+  exit 0;
 fi
 
 check_if_exists "$NANOCFG"
-check_if_exists "$DATASET_FILE"
 check_if_exists "$JSON_LUMI"
-check_if_exists "$PRIVATE_MINIAOD_PATH"
 
 # Saving absolute path
 if [[ ! "$DATASET_FILE" =~ ^/ ]]; then
@@ -204,14 +222,14 @@ echo "Checking if crab is available ..."
 CRAB_AVAILABLE=$(which crab 2>/dev/null)
 if [ -z "$CRAB_AVAILABLE" ]; then
   echo "crab not available! please do: source /cvmfs/cms.cern.ch/crab3/crab.sh"
-  exit 3;
+  exit 1;
 fi
 
 echo "Checking if VOMS is available ..."
 VOMS_PROXY_AVAILABLE=$(which voms-proxy-info 2>/dev/null)
 if [ -z "$VOMS_PROXY_AVAILABLE" ]; then
   echo "VOMS proxy not available! please do: source /cvmfs/grid.cern.ch/glite/etc/profile.d/setup-ui-example.sh";
-  exit 4;
+  exit 1;
 fi
 
 echo "Checking if VOMS is open long enough ..."
@@ -221,7 +239,7 @@ VOMS_PROXY_TIMELEFT=$(voms-proxy-info --timeleft)
 if [ "$VOMS_PROXY_TIMELEFT" -lt "$MIN_TIMELEFT" ]; then
   echo "Less than $MIN_HOURSLEFT hours left for the proxy to be open: $VOMS_PROXY_TIMELEFT seconds";
   echo "Please update your proxy: voms-proxy-init -voms cms -valid 192:00";
-  exit 5;
+  exit 1;
 fi
 
 export DATASET_PATTERN="^/(.*)/(.*)/[0-9A-Za-z]+$"
@@ -275,19 +293,19 @@ cat $DATASET_FILE | while read LINE; do
 
   if [ "$DATASET_THIRD_PART" == "USER" ]; then
     echo "It's a privately produced sample";
-    PRIVATE_DATASET_PATH="$PRIVATE_MINIAOD_PATH/$DATASET_LEADING_PART";
+    PRIVATE_DATASET_PATH=$(echo $LINE | awk '{print $6}');
     if [[ "$PRIVATE_DATASET_PATH" != /hdfs/cms/* ]]; then
       echo "The path to private datasets must start with /hdfs/cms";
-      exit 6;
+      exit 1;
     fi
     if [[ ! -d "$PRIVATE_DATASET_PATH" ]]; then
       echo "Invalid path for dataset $DATASET: $PRIVATE_DATASET_PATH";
-      exit 7;
+      exit 1;
     fi
     export PRIVATE_DATASET_FILES=`find $PRIVATE_DATASET_PATH -type f -name '*.root'`;
     if [ -z "$PRIVATE_DATASET_FILES" ]; then
       echo "No files found in $PRIVATE_DATASET_PATH";
-      exit 8;
+      exit 1;
     fi
     echo "Found the following files in $DATASET:"
     for PRIVATE_DATASET_FILE in $PRIVATE_DATASET_FILES; do
