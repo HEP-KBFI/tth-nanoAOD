3749c1d21e7bb1091847f73db332377f509c745f
ktht <karlehataht@gmail.com>
2019-04-10 03:11:03 +0300

    Log more information about the memory consumption

--- python/debug.py
+++ python/debug.py
@@ -3,9 +3,11 @@ import FWCore.ParameterSet.Config as cms
 def debug(process, dump = False, dumpFile = 'nano.dump', tracer = False, memcheck = False):
   if memcheck:
     process.SimpleMemoryCheck = cms.Service("SimpleMemoryCheck",
-      ignoreTotal         = cms.untracked.int32(1),
-      oncePerEventMode    = cms.untracked.bool(True),
-      moduleMemorySummary = cms.untracked.bool(True),
+      ignoreTotal          = cms.untracked.int32(1),
+      oncePerEventMode     = cms.untracked.bool(True),
+      moduleMemorySummary  = cms.untracked.bool(True),
+      showMallocInfo       = cms.untracked.bool(True),
+      monitorPssAndPrivate = cms.untracked.bool(True),
     )
   if tracer:
     process.Tracer = cms.Service("Tracer",
