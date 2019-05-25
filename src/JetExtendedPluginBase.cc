3683684217bbd561a7588989f6c126cf2aceee50
ktht <karlehataht@gmail.com>
2019-05-26 00:46:35 +0300

    Remove JetExtendedPluginBase (deprecated)

--- src/JetExtendedPluginBase.cc
+++
@@ -1,7 +0,0 @@
-#include "tthAnalysis/NanoAOD/interface/JetExtendedPluginBase.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-EDM_REGISTER_PLUGINFACTORY(JetExtendedPluginFactory, "JetExtendedPluginFactory");
-
-
