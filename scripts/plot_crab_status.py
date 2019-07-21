e3f0ecbed5f0e3a88796d11c4887770ae4151872
ktht <karlehataht@gmail.com>
2019-07-22 00:08:08 +0300

    Compile status of each CRAB task into an HTML document

--- scripts/plot_crab_status.py
+++ scripts/plot_crab_status.py
@@ -25,8 +25,8 @@ html_template = """
   <head>
     <meta charset="utf-8">
 
-    <title>CRAB progress</title>
-    <meta name="description" content="CRAB progress">
+    <title>{{ title }}</title>
+    <meta name="description" content="{{ title }}">
 
     <style>
     </style>
@@ -95,7 +95,17 @@ def parse_lines(lines):
     assert(not parsed)
   return parsed, current_datetime
 
-def plot_pie(all_states, output, nof_tasks, nof_completed_tasks, title):
+def encode_image(fig):
+  tmp_image = tempfile.TemporaryFile(suffix = 'png')
+  fig.savefig(tmp_image, bbox_inches = 'tight', format = 'png')
+  plt.close()
+
+  tmp_image.seek(0)
+  encoded_image = base64.b64encode(tmp_image.read())
+  tmp_image.close()
+  return encoded_image
+
+def plot_pie(all_states, title, figsize):
   vals   = list(map(lambda kv: kv[1], all_states))
   labels = list(map(lambda kv: kv[0], all_states))
   cols   = list(map(lambda lab: COLMAP[lab], labels))
@@ -103,11 +113,11 @@ def plot_pie(all_states, output, nof_tasks, nof_completed_tasks, title):
   tot = sum(vals)
   legend_labels = list(map(lambda kv: '%s %.2f%%' % (kv[0], 100. * kv[1] / tot), all_states))
 
-  fig, ax = plt.subplots(figsize = (10, 8), subplot_kw = dict(aspect = "equal"))
+  fig, ax = plt.subplots(figsize = figsize, subplot_kw = dict(aspect = "equal"))
   wedges, texts = ax.pie(vals, startangle = 60, colors = cols)
 
   bbox_props = dict(boxstyle = "square,pad=0.3", fc = "w", ec = "k", lw = 0.72)
-  kw = dict(arrowprops = dict(arrowstyle = "-"), bbox = bbox_props, zorder = 0, va = "center", fontsize = 12)
+  kw = dict(arrowprops = dict(arrowstyle = "-"), bbox = bbox_props, zorder = 0, va = "center", fontsize = 10)
 
   for i, p in enumerate(wedges):
     ang = (p.theta2 - p.theta1) / 2. + p.theta1
@@ -121,26 +131,17 @@ def plot_pie(all_states, output, nof_tasks, nof_completed_tasks, title):
     ax.annotate(
       str(vals[i]), xy = (x, y), xytext = (1.35 * np.sign(x), 1.4 * y), horizontalalignment = horizontalalignment, **kw
     )
-  plt.legend(
+  ax.legend(
     wedges, legend_labels, loc = 'lower left', title = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),
-    prop = { 'size' : 12 },
+    fontsize = 10
   )
-
-  ax.set_title(
-    "{}{} jobs in {} tasks ({} completed)".format('%s: ' % title if title else '',  tot, nof_tasks, nof_completed_tasks)
-  )
-
-  plt.savefig(output, bbox_inches = 'tight')
-  logging.info("Saved figure to: {}".format(output))
+  ax.set_title("{}{} job(s)".format('%s: ' % title,  tot))
   plt.close()
 
-def plot_progress(data):
-  all_states = list({state for entry in data for state in entry[0].keys()})
-  available_dates = list(sorted(d[1] for d in data))
+  return fig
 
-  current_date = datetime.datetime.now()
-  first_date = available_dates[0] if available_dates else current_date
-  first_date = first_date.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+def plot_progress(data, first_date, current_date):
+  all_states = list({state for entry in data for state in entry[0].keys()})
 
   difference_in_days = (current_date - first_date).days
   data_augmented = {
@@ -186,29 +187,24 @@ def plot_progress(data):
       data_to_plot_row.append(data_augmented[date]['data'][state])
     data_to_plot.append(data_to_plot_row)
 
-  plt.figure(figsize = (5.5, 3.5))
-  plt.ylabel('Number of CRAB jobs')
+  fig, ax = plt.subplots(figsize = (5.5, 3.5))
+  ax.set_ylabel('Number of CRAB jobs')
   if prev_date:
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-    plt.stackplot(dates_sorted, data_to_plot, colors = [ COLMAP[state] for state in all_states ], labels = all_states)
-    plt.gcf().autofmt_xdate()
-    plt.legend(loc = 'upper left', fontsize = 10)
-    plt.gca().set_xticks(plt.gca().get_xticks()[::2])
-    plt.gca().tick_params(axis = 'both', labelsize = 10)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    ax.xaxis.set_major_locator(mdates.DayLocator())
+    ax.stackplot(dates_sorted, data_to_plot, colors = [ COLMAP[state] for state in all_states ], labels = all_states)
+    fig.autofmt_xdate()
+    ax.legend(loc = 'upper left', fontsize = 10)
+    ax.set_xticks(plt.gca().get_xticks()[::2])
+    ax.tick_params(axis = 'both', labelsize = 10)
 
     nof_jobs = sum(col[-1] for col in data_to_plot)
-    plt.ylim(0, nof_jobs)
+    ax.set_ylim(0, nof_jobs)
 
-  tmp_image = tempfile.TemporaryFile(suffix = 'png')
-  plt.savefig(tmp_image, bbox_inches = 'tight', format = 'png')
+  encoded_image = encode_image(fig)
   plt.close()
 
-  tmp_image.seek(0)
-  encoded_image = base64.b64encode(tmp_image.read())
-  tmp_image.close()
-
-  return encoded_image, first_date
+  return encoded_image
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
@@ -232,6 +228,10 @@ if __name__ == '__main__':
     help = 'R|Additional title to the plot',
   )
   parser.add_argument(
+    '-d', '--detailed-plots', dest = 'detailed_plots', metavar = 'file', required = False, type = str, default = '',
+    help = 'R|Detailed plots'
+  )
+  parser.add_argument(
     '-P', '--progress', dest = 'progress', metavar = 'file', required = False, type = str, default = '',
     help = 'R|HTML file showing the progress of each CRAB task',
   )
@@ -247,11 +247,13 @@ if __name__ == '__main__':
     format = '%(asctime)s - %(levelname)s: %(message)s',
   )
 
-  in_dir   = args.input
-  pattern  = re.compile(args.pattern)
-  output   = args.output
-  title    = args.title
-  progress = os.path.abspath(args.progress)
+  in_dir      = args.input
+  pattern     = re.compile(args.pattern)
+  output      = os.path.abspath(args.output)
+  title       = args.title
+  progress    = os.path.abspath(args.progress) if args.progress else ''
+  do_detailed = os.path.abspath(args.detailed_plots) if args.detailed_plots else ''
+  assert(os.path.isdir(os.path.dirname(output)))
 
   lines = {}
   for subdir in os.listdir(in_dir):
@@ -298,6 +300,16 @@ if __name__ == '__main__':
     else:
       logging.error("No job statistics for task: {}".format(subdir))
 
+  available_dates = {}
+  first_dates = {}
+  current_date = datetime.datetime.now()
+  for subdir in lines:
+    available_dates_subdir = list(sorted(entry[1] for entry in lines[subdir]))
+    first_date = available_dates_subdir[0] if available_dates_subdir else current_date
+
+    available_dates[subdir] = available_dates_subdir
+    first_dates[subdir] = first_date.replace(hour = 0, minute = 0, second = 0, microsecond = 0)
+
   if progress:
     progress_dir = os.path.dirname(progress)
     if not os.path.isdir(progress_dir):
@@ -305,16 +317,18 @@ if __name__ == '__main__':
 
     encoded_images = []
     for subdir in sorted(lines.keys()):
-      encoded_image, first_date = plot_progress(lines[subdir])
-      encoded_images.append((subdir, encoded_image, first_date))
+      encoded_image = plot_progress(lines[subdir], first_dates[subdir], current_date)
+      encoded_images.append((subdir, encoded_image, first_dates[subdir]))
     encoded_images = list(sorted(encoded_images, key = lambda entry: entry[2]))
 
     with open(progress, 'w') as progress_file:
       html = jinja2.Template(html_template).render(
+        title = "CRAB progress",
         images = encoded_images,
         current_time = str(datetime.datetime.now()),
       )
       progress_file.write(html)
+    logging.info('Saved file: {}'.format(progress))
 
   nof_completed_tasks = len(
     filter(lambda kv: len(kv[1]) > 0 and len(kv[1][-1][0]) == 1 and 'finished' in kv[1][-1][0], lines.items())
@@ -330,4 +344,28 @@ if __name__ == '__main__':
       all_states[state] += values
 
   all_states_sorted = sorted(all_states.items(), key = lambda kv: kv[1], reverse = True)
-  plot_pie(all_states_sorted, output, len(lines), nof_completed_tasks, title)
+  title_agg = '{}{} tasks ({} completed)'.format('%s - ' % title if title else '', len(lines), nof_completed_tasks)
+  pie_figure = plot_pie(all_states_sorted, title_agg, figsize = (10, 8))
+  pie_figure.savefig(output, bbox_inches = 'tight')
+  logging.info("Saved figure to: {}".format(output))
+
+  if do_detailed:
+    encoded_images = []
+    for subdir in sorted(lines.keys()):
+      all_lines = lines[subdir]
+      subdir_states = all_lines[-1][0] if all_lines else {}
+      subdir_states_sorted = sorted(subdir_states.items(), key = lambda kv: kv[1], reverse = True)
+      pie_figure = plot_pie(subdir_states_sorted, title, figsize = (4.5, 4.5))
+      encoded_image = encode_image(pie_figure)
+      plt.close()
+      encoded_images.append((subdir, encoded_image, first_dates[subdir]))
+    encoded_images = list(sorted(encoded_images, key = lambda entry: entry[2]))
+
+    with open(do_detailed, 'w') as detailed_file:
+      html = jinja2.Template(html_template).render(
+        title = "CRAB details",
+        images = encoded_images,
+        current_time = str(datetime.datetime.now()),
+      )
+      detailed_file.write(html)
+    logging.info('Saved file: {}'.format(do_detailed))
