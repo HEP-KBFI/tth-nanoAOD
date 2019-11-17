57903b7bec05135a83e3f5f9c6ca0224c6eea7fa
ktht <karlehataht@gmail.com>
2019-11-17 22:52:29 +0200

    Check differences in run-lumi nrs against golden JSON

--- scripts/find_nano_datasets.py
+++ scripts/find_nano_datasets.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from FWCore.PythonUtilities.LumiList import LumiList
+
 import argparse
 import logging
 import sys
@@ -18,6 +20,32 @@ logging.basicConfig(
   level  = logging.INFO,
   format = '%(asctime)s - %(levelname)s: %(message)s'
 )
+
+GOLDEN_JSONS = {
+  '2016' : 'Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt',
+  '2017' : 'Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt',
+  '2018' : 'Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt',
+}
+
+# see https://twiki.cern.ch/twiki/bin/view/CMS/PdmV2018AnalysisMissingMiniAODPrompt
+MINIAOD_RUNLUMI_EXCEPTIONS = [
+  '/EGamma/Run2018D-PromptReco-v2/MINIAOD', # 171 files corrupted
+  '/SingleMuon/Run2018D-PromptReco-v2/MINIAOD', # 73 files corrupted
+  '/Tau/Run2018D-PromptReco-v2/MINIAOD', # 11 files corrupted
+]
+
+def load_golden_jsons(golden_json_path):
+  if not os.path.isfile(golden_json_path):
+    raise RuntimeError("No such file: %s" % golden_json_path)
+  lumi_obj = LumiList(golden_json_path)
+  lumis = lumi_obj.getLumis()
+  runlumi_dict = {}
+  for run, lumi in lumis:
+    if run not in runlumi_dict:
+      runlumi_dict[run] = []
+    assert(lumi not in runlumi_dict[run])
+    runlumi_dict[run].append(lumi)
+  return runlumi_dict
 
 class SmartFormatter(argparse.HelpFormatter):
   def _split_lines(self, text, width):
@@ -179,68 +207,120 @@ def get_runlumi(dbs_name):
     runlumis[run] = lumis
   return runlumis
 
-def runlumi_match(miniaod, nanoaod, nanoaod_status):
+def convert_to_ranges(lumilist):
+  lumilist = sorted(set(lumilist))
+  lumi_min = min(lumilist)
+  lumi_max = max(lumilist)
+  if lumi_max in [ lumi_min, lumi_min + 1 ]:
+    return [ [ lumi_min, lumi_max ] ]
+  lumi_ranges = []
+  lumi_first = lumi_min
+  lumi_last = lumi_min
+  for lumi in lumilist[1:]:
+    if lumi != (lumi_last + 1) or lumi == lumi_max:
+      lumi_ranges.append([ lumi_first, lumi_last ])
+      lumi_first = lumi
+    lumi_last = lumi
+  return str(lumi_ranges).replace(' ', '')
+
+def runlumi_match(miniaod, nanoaod, nanoaod_status, golden_json):
   if miniaod.endswith('SIM'):
     assert(nanoaod.endswith('SIM'))
     return True
   else:
     assert(not nanoaod.endswith('SIM'))
-  is_compatible = True
+  if nanoaod_status == 'PRODUCTION':
+    logging.warning("Dataset {} is in production".format(nanoaod))
+    return True
 
   miniaod_size = get_size(miniaod)
   nanoaod_size = get_size(nanoaod)
-  if nanoaod_size < miniaod_size:
+  if nanoaod_size > miniaod_size:
+    RuntimeError(
+      "Dataset %s has more events (%d) than dataset %s (%s)" % \
+      (nanoaod, nanoaod_size, miniaod, miniaod_size)
+    )
+  elif nanoaod_size < miniaod_size:
     logging.error(
       "The number of events in dataset {} ({}) does not match to the number of events in dataset {} ({})".format(
         miniaod, miniaod_size, nanoaod, nanoaod_size
       )
     )
-    is_compatible = False
-  elif nanoaod_size > miniaod_size:
-    RuntimeError(
-      "Dataset %s has more events (%d) than dataset %s (%s)" % \
-      (nanoaod, nanoaod_size, miniaod, miniaod_size)
-    )
+    retval = True
 
-  runlumi_miniaod = get_runlumi(miniaod)
-  runlumi_nanoaod = get_runlumi(nanoaod)
-  run_missing_miniaod = set(runlumi_nanoaod.keys()) - set(runlumi_miniaod.keys())
-  if run_missing_miniaod:
-    raise RuntimeError(
-      "Found %d run numbers that are present in %s but not in %s: %s" % \
-      (len(run_missing_miniaod), nanoaod, miniaod, ', '.join(map(str, list(run_missing_miniaod))))
-    )
-  run_missing_nanoaod = set(runlumi_miniaod.keys()) - set(runlumi_nanoaod.keys())
-  if run_missing_nanoaod:
-    logging.error(
-      "Found {} run numbers that are present in {} but not in {}: {}".format(
-        len(run_missing_nanoaod), miniaod, nanoaod, ', '.join(map(str, list(run_missing_nanoaod)))
-      )
-    )
-    is_compatible = False
-  for run in runlumi_miniaod:
-    if run not in runlumi_nanoaod:
-      continue
-    lumis_miniaod = runlumi_miniaod[run]
-    lumis_nanoaod = runlumi_nanoaod[run]
-    lumis_missing_miniaod = lumis_nanoaod - lumis_miniaod
-    if lumis_missing_miniaod:
+    runlumi_miniaod = get_runlumi(miniaod)
+    runlumi_nanoaod = get_runlumi(nanoaod)
+    run_missing_miniaod = set(runlumi_nanoaod.keys()) - set(runlumi_miniaod.keys())
+    if run_missing_miniaod:
       raise RuntimeError(
-        "Found lumis at run %d that are present in %s but not in %s: %s" % \
-        (run, nanoaod, miniaod, ', '.join(map(str, list(lumis_missing_miniaod))))
+        "Found %d run numbers that are present in %s but not in %s: %s" % \
+        (len(run_missing_miniaod), nanoaod, miniaod, ', '.join(map(str, list(run_missing_miniaod))))
       )
-    lumis_missing_nanoaod = lumis_miniaod - lumis_nanoaod
-    if lumis_missing_nanoaod:
+    run_missing_nanoaod = sorted(list(set(runlumi_miniaod.keys()) - set(runlumi_nanoaod.keys())))
+    if run_missing_nanoaod:
       logging.error(
-        "Found lumis at run {} that are present in {} but not in {}: {}".format(
-          run, miniaod, nanoaod, ', '.join(map(str, list(lumis_missing_nanoaod)))
+        "Found {} run numbers that are present in {} but not in {}: {}".format(
+          len(run_missing_nanoaod), miniaod, nanoaod, ', '.join(map(str, run_missing_nanoaod))
         )
       )
-      is_compatible = False
-
-  if not is_compatible and nanoaod_status != 'PRODUCTION':
-    logging.error("Dataset {} is not in production".format(nanoaod))
-    return False
+    run_missing_nanoaod_golden = [ run for run in run_missing_nanoaod if run in golden_json ]
+    run_missing_nanoaod_not_golden = [ run for run in run_missing_nanoaod if run not in golden_json ]
+    if run_missing_nanoaod_golden:
+      logging.error(
+        "The following in golden JSON but not in {}: {}".format(
+          nanoaod, ', '.join(map(str, run_missing_nanoaod_golden))
+        )
+      )
+      retval = False
+    if run_missing_nanoaod_not_golden:
+      logging.info(
+        "The following runs missing in {} not golden: {}".format(
+          nanoaod, ', '.join(map(str, run_missing_nanoaod_not_golden))
+        )
+      )
+    for run in runlumi_miniaod:
+      if run not in golden_json:
+        continue
+      if run not in runlumi_nanoaod:
+        continue
+      lumis_miniaod = runlumi_miniaod[run]
+      lumis_nanoaod = runlumi_nanoaod[run]
+      lumis_missing_miniaod = sorted(list(lumis_nanoaod - lumis_miniaod))
+      if lumis_missing_miniaod:
+        raise RuntimeError(
+          "Found lumis at run %d that are present in %s but not in %s: %s" % \
+          (run, nanoaod, miniaod, convert_to_ranges(lumis_missing_miniaod))
+        )
+      lumis_missing_nanoaod = sorted(list(lumis_miniaod - lumis_nanoaod))
+      if lumis_missing_nanoaod:
+        logging.error(
+          "Found lumis at run {} that are present in {} but not in {}: {}".format(
+            run, miniaod, nanoaod, convert_to_ranges(lumis_missing_nanoaod)
+          )
+        )
+      lumis_missing_nanoaod_golden = [ lumi for lumi in lumis_missing_nanoaod if lumi in golden_json[run] ]
+      lumis_missing_nanoaod_not_golden = [ lumi for lumi in lumis_missing_nanoaod if lumi not in golden_json[run] ]
+      if lumis_missing_nanoaod_golden:
+        logging.error(
+          "The following lumis at run {} in golden JSON but not in {}: {}".format(
+            run, nanoaod, convert_to_ranges(lumis_missing_nanoaod_golden)
+          )
+        )
+        retval = False
+      if lumis_missing_nanoaod_not_golden:
+        logging.error(
+          "The following lumis at run {} missing in {} not golden: {}".format(
+            run, nanoaod, convert_to_ranges(lumis_missing_nanoaod_not_golden)
+          )
+        )
+    if not retval and miniaod in MINIAOD_RUNLUMI_EXCEPTIONS:
+      logging.info(
+        "Dataset {} has corrupted files, which is the reason why {} is missing events".format(miniaod, nanoaod)
+      )
+      retval = True
+    return retval
+  else:
+    logging.info("Number of events in {} match to the number of events in {}: {}".format(miniaod, nanoaod, miniaod_size))
   return True
 
 def find_matching_nano(miniaods, dbs_nano, data_str, mc_str):
@@ -324,6 +404,11 @@ dbs_mc = get_nanoaod(args.mc, False)
 dbs_nano = merge_dicts(dbs_data, dbs_mc)
 logging.info('Considering {} dataset(s) in total'.format(len(dbs_nano)))
 
+json_base_path = os.path.join(os.environ['CMSSW_BASE'], 'src', 'tthAnalysis', 'NanoAOD', 'data')
+golden_jsons = {}
+for era in GOLDEN_JSONS:
+  golden_jsons['Run{}'.format(era)] = load_golden_jsons(os.path.join(json_base_path, GOLDEN_JSONS[era]))
+
 for input_file in data:
   miniaods = list(map(lambda line_out: line_out[1], filter(lambda line_in: len(line_in) > 1, data[input_file])))
   nanoaod_parents = find_matching_nano(miniaods, dbs_nano, args.data, args.mc)
@@ -339,7 +424,16 @@ for input_file in data:
       elif len(line) == 2:
         miniaod = line[1]
         nanoaod_cand = nanoaods[miniaod]
-        if nanoaod_cand and runlumi_match(miniaod, nanoaod_cand, dbs_nano[nanoaod_cand]):
+        if not nanoaod_cand.endswith('SIM'):
+          golden_json_eras = [ era for era in golden_jsons if era in nanoaod_cand ]
+          if len(golden_json_eras) != 1:
+            raise RuntimeError("Unable to find golden JSON for %s" % nanoaod_cand)
+          golden_json_era = golden_json_eras[0]
+          logging.info("Golden JSON era for dataset {}: {}".format(nanoaod_cand, golden_json_era))
+          golden_json = golden_jsons[golden_json_era]
+        else:
+          golden_json = {}
+        if nanoaod_cand and runlumi_match(miniaod, nanoaod_cand, dbs_nano[nanoaod_cand], golden_json):
           line_remaining = line[0].replace(miniaod, '').lstrip()
           f.write('{}{}\n'.format(nanoaod_cand.ljust(max_width_nanoaod), line_remaining))
         else:
